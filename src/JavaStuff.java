9d58804090d124f5b8648758ec8fc7feee73a498
James Spicer <spicer.r.james@gmail.com>
2020-04-03 15:05:19 -0500

    second push

--- src/JavaStuff.java
+++ src/JavaStuff.java
@@ -1,5 +1,24 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
 public class JavaStuff {
-    public static void main(final String[] args){
-        System.out.println("Hello World");
-    }
-}
+
+    public static void main(final String[] args) throws IOException {
+//        System.out.println("Hello World");
+                try {
+                    File myObj = new File("/Users/jamesspicer/Desktop/TestFolder");
+                    if (myObj.createNewFile()) {
+                        System.out.println(myObj.getName() + " Has been created");
+                    } else {
+                        System.out.println("File already exists");
+                    }
+                } catch(IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+
+
+
+
