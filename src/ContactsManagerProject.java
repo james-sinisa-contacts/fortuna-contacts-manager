ebd9755ae5fbf40a58408b35170fd44a15ab9738
Sinisa Tesic <sinisa.tesic210@gmail.com>
2020-04-06 14:26:32 -0500

    commented out in progress content

--- src/ContactsManagerProject.java
+++ src/ContactsManagerProject.java
@@ -7,8 +7,7 @@ import java.nio.file.Paths;
 import java.sql.SQLOutput;
 import java.util.List;
 import java.util.Scanner;
-
-import static java.nio.file.Paths.get;
+//import static java.nio.file.Paths.get;
 
 public class ContactsManagerProject {
 
@@ -18,62 +17,87 @@ public class ContactsManagerProject {
         displayInfo();
 
     }
+    //end of main
 
 
-
-//home "page" method
+    //home "page" method
     public static void displayInfo() throws IOException {
-        System.out.println("\nWelcome to da contactzzz\n");
+        System.out.println("Welcome to da contactzzz\n");
 
         Scanner menuInput = new Scanner(System.in);
 
         int userInput;
         boolean displayInfo = true;
 
-        do{
-        System.out.println("Select an option below:");
-        System.out.println("\t[1] View contacts.");
-        System.out.println("\t[2] Add a new contact.");
-        System.out.println("\t[3] Search a contact by name.");
-        System.out.println("\t[4] Delete an existing contact.");
-        System.out.println("\t[5] Exit program.");
-        userInput = menuInput.nextInt();
+        do {
+            System.out.println("Select an option below:");
+            System.out.println("\t[1] View contacts.");
+            System.out.println("\t[2] Add a new contact.");
+            System.out.println("\t[3] Search a contact by name.");
+            System.out.println("\t[4] Delete an existing contact.");
+            System.out.println("\t[5] Exit program.");
+            userInput = menuInput.nextInt();
 
-        if (userInput == 1) {
-            System.out.println("\n\tContacts are listed below:");
-            displayContacts();
-            displayInfo = false;
-        } else if (userInput == 2) {
-            System.out.println("\n\tPlease enter name of new contact:");
-            displayInfo = false;
-        } else if (userInput == 3) {
-            System.out.println("\n\tEnter contact name below to begin search:");
-            displayInfo = false;
-        } else if (userInput == 4) {
-            System.out.println("\n\tWhich contact would you like to delete?");
-            displayInfo = false;
-        } else if (userInput == 5) {
-            System.out.println("\n\tGoodbaiii");
-            displayInfo = false;
-        } else {
-            System.out.println("Not a valid input; please select from inputs 1 through 5");
+            if (userInput == 1) {
+                System.out.println("\n\tContacts are listed below:");
+                displayContacts();
+                displayInfo = false;
+            } else if (userInput == 2) {
+                System.out.println("\n\tPlease enter name of new contact:");
+                displayInfo = false;
+            } else if (userInput == 3) {
+                System.out.println("\n\tEnter contact name below to begin search:");
+                displayInfo = false;
+            } else if (userInput == 4) {
+                System.out.println("\n\tWhich contact would you like to delete?");
+                displayInfo = false;
+            } else if (userInput == 5) {
+                System.out.println("\n\tGoodbaiii");
+                displayInfo = false;
+            } else {
+                System.out.println("Not a valid input; please select from inputs 1 through 5");
 
-        }
+            }
         } while (displayInfo);
     }
 
 
-
+    //Option 1 method:
     public static void displayContacts() throws IOException {
-
-        //
         Path contactPath = Paths.get("data", "contacts.txt");
-        if (!Files.exists(contactPath)){
+        if (!Files.exists(contactPath)) {
             throw new IOException("File doesn't exist");
         }
 
         List<String> fileLines = Files.readAllLines(contactPath);
         System.out.println(fileLines);
-
     }
+
+
+//Option 2 method:
+
+
+
+
+    //Option 3 method:
+//    public static void searchContacts() {
+//        Path contactPath = Paths.get("data", "contacts.txt");
+//        Scanner search = new Scanner(System.in);
+//        boolean searching = true;
+//        String input;
+//
+//        do {
+//            System.out.println("Enter name you're searching for: ");
+//            input = search.nextLine();
+//
+//            if (contactPath.equals(input)) {
+//                System.out.println(contactPath.startsWith(input));
+//                searching = false;
+//            } else {
+//                System.out.println("Name not found, try again");
+//            }
+//        } while (searching);
+//
+//    }
+
 }
